47db060de200c156fafd11593d8ff0a49d410d74
rui.zheng <rui.zheng@tcl.com>
2014-02-19 14:17:48 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+// main
+package main
+
+import (
+	"github.com/ginuerzh/weixin/mp"
+	"log"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func main() {
+	wx := mp.New("wxb6ebfdef79a09651", "abd7052dc840112a9fbffe39e6fbeaca", "1234567890")
+	wx.Init("/")
+
+	wx.HandleFunc(mp.MsgTypeText, func(reply mp.MessageReplyer, m *mp.Message) {
+		if err := reply.ReplyText("今日自驾: " + m.Content); err != nil {
+			log.Println(err)
+		}
+	})
+	wx.Run(8081)
+}
